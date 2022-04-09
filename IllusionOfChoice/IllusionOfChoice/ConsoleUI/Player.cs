fb8b6b04c75af5244dd020b4b34eec06bc3d229d
Waffas1138 <bivinst5220@student.faytechcc.edu>
2022-04-09 15:24:56 -0400

    c

--- IllusionOfChoice/IllusionOfChoice/ConsoleUI/Player.cs
+++ IllusionOfChoice/IllusionOfChoice/ConsoleUI/Player.cs
@@ -97,7 +97,6 @@ namespace ConsoleUI
             int random atk = new Random(1,100);
             Console.WriteLine(hp - atk);
             console.writeLine("Testing for damage")
-            console.writeline(""   );
         }
     }
 
